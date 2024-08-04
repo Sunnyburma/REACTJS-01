116923e1acbaca97e10b71a16c0fa5dde3cb5bbf
sunny burman <sunnyb142@gmail.com>
2024-08-04 20:48:26 +0530

    code

--- crud-app/crud-app/src/actions/actions.js
+++ crud-app/crud-app/src/actions/actions.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
 const actions =()=>{
     return async (dispatch)=>{
-        dispatch({})
+        dispatch({
+            type: "GET_PRODUCTS",
+            loading:false,
+            products:[],
+            error:"",
+            })
     }
 }
